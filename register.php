ead1468a016b6d0820da09bff50637ea91ac4786
Bagasss08 <bwidyastama@gmail.com>
2025-03-28 16:57:41 +0700

    register

--- register.php
+++ register.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="login.css">
-    <title>Sign In</title>
+    <link rel="stylesheet" href="register.css">
+    <title>Create an account</title>
 </head>
 <body>
     <div class="container">
-        <h1>Sign in to your account</h1>
-        <p>Enter your email to sign in for this app</p>
+        <h1>Create an account</h1>
+        <p>Enter your email to Create account</p>
         <form action="#">
             <label for="email">Email</label>
             <input type="email" id="email" name="email" placeholder="email@domain.com" required>
@@ -17,9 +17,9 @@
             <label for="password">Password</label>
             <input type="password" id="password" name="password" placeholder="Password" required>
 
-            <button type="submit">Sign in with email</button>
+            <button type="submit">Create account with email</button>
         </form>
-        <p><a href="register.php" class="account-link">Create an account</a></p>
+        <!-- <p><a href="register.html" class="account-link">Create an account</a></p> -->
         <p>By clicking continue, you agree to our <a href="#" class="terms-link">Terms of Service</a> and <a href="#" class="privacy-link">Privacy Policy</a>.</p>
     </div>
 </body>
