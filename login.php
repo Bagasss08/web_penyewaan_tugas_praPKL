4adf17a400d86d6ad33abb9c17f35ac2d2d3c285
Bagasss08 <bwidyastama@gmail.com>
2025-03-28 15:47:00 +0700

    koneksi

--- login.php
+++ login.php
@@ -8,14 +8,18 @@
 </head>
 <body>
     <div class="container">
-        <h1>Sign in account</h1>
+        <h1>Sign in to your account</h1>
         <p>Enter your email to sign in for this app</p>
-        <form>
-            <input type="email" placeholder="email@domain.com" required>
-            <input type="password" placeholder="Password" required>
-            <button type="submit">Sign up with email</button>
+        <form action="#">
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" placeholder="email@domain.com" required>
+
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" placeholder="Password" required>
+
+            <button type="submit">Sign in with email</button>
         </form>
-        <p><a href="#" class="account-link">No Account?</a></p>
+        <p><a href="register.html" class="account-link">Create an account</a></p>
         <p>By clicking continue, you agree to our <a href="#" class="terms-link">Terms of Service</a> and <a href="#" class="privacy-link">Privacy Policy</a>.</p>
     </div>
 </body>
